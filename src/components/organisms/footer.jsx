f6194a26de29d944e0e75c547469c9b1a3096ef8
Lutfan Hadi <hadilutfan3@gmail.com>
2025-04-17 19:13:57 +0700

    memperbaiki footer di semua perangkat

--- src/components/organisms/footer.jsx
+++ src/components/organisms/footer.jsx
@@ -3,37 +3,39 @@ import { Email, Facebook, Instagram, Phone, Send, Twitter, Youtube } from "../at
 export const Footer = () => {
   return (
     <div className="bg-[#232233] relative mt-36">
-      <div className="bg-white rounded-xl text-center w-fit left-1/2 -translate-x-1/2 px-12 py-7 drop-shadow-2xl absolute -top-[15.5%]">
-        <div className="flex flex-col items-center gap-5">
+      <div className="flex flex-col bg-white rounded-xl text-center w-fit left-1/2 -translate-x-1/2 px-10 py-7 drop-shadow-2xl absolute -top-[15.5%] md:-top-[17%] md:py-16 md:justify-center md:items-center md:flex-row md:w-[90%] md:mx-auto lg:w-[80%]">
+        <div className="flex flex-col items-center gap-5 md:flex-row md:border-r-4 md:pr-5 lg:pr-10">
           <div className="bg-primary p-[15px] rounded-full text-white">
             <Email />
           </div>
-          <p className="text-xl font-medium border-b-4 pb-5 uppercase">
+          <p className="text-xl font-medium border-b-4 pb-5 uppercase md:pb-0 md:border-b-0 md:text-[16px] lg:text-xl">
             info.design@gmail.com
           </p>
         </div>
-        <div className="flex flex-col items-center gap-5 mt-7">
+        <div className="flex flex-col items-center gap-5 mt-7 md:mt-0 md:pl-5 md:flex-row lg:pl-10">
           <div className="bg-primary p-[15px] rounded-full text-white">
             <Phone />
           </div>
-          <p className="text-xl font-medium">+880 321 655 9985</p>
+          <p className="text-xl font-medium md:text-[16px] lg:text-xl">
+            +880 321 655 9985
+          </p>
         </div>
       </div>
-      <div className="text-white pt-56 flex flex-col gap-14 mx-4">
-        <div>
+      <div className="text-white pt-56 flex flex-col gap-14 mx-4 md:flex-row md:mx-10 md:justify-between md:pt-48 lg:mx-[135px]">
+        <div className="md:w-[213px] lg:w-[270px]">
           <h1 className="text-2xl font-bold">Logo</h1>
           <p className="font-normal text-[16px] pt-2.5">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra
             nunc ante velit vitae. Est tellus vitae, nullam lobortis enim.
           </p>
-          <div className="flex gap-[30px] pt-9">
-            <div className="border-r-2 pr-[30px]">
+          <div className="flex gap-[30px] pt-9 md:gap-5 lg:gap-7">
+            <div className="border-r-2 pr-[30px] md:pr-5 lg:pr-7">
               <Facebook />
             </div>
-            <div className="border-r-2 pr-[30px]">
+            <div className="border-r-2 pr-[30px] md:pr-5 lg:pr-7">
               <Instagram />
             </div>
-            <div className="border-r-2 pr-[30px]">
+            <div className="border-r-2 pr-[30px] md:pr-5 lg:pr-7">
               <Twitter />
             </div>
             <div>
@@ -58,7 +60,7 @@ export const Footer = () => {
             </li>
           </ul>
         </div>
-        <div>
+        <div className="md:w-[213px] lg:w-[270px]">
           <h1 className="text-2xl font-bold">news letter</h1>
           <p className="font-normal text-[16px] pt-2.5 ">
             Subscribe our newsletter to get our latest update & news
@@ -76,8 +78,10 @@ export const Footer = () => {
           </div>
         </div>
       </div>
-      <footer className="mt-14 mx-4">
-        <p className="text-white text-center text-[16px] py-6 border-t-2">&copy; Copyright 2021 .Ojjomedia. All Right Reserved.</p>
+      <footer className="mt-14 mx-4 md:mx-10 lg:mx-[135px]">
+        <p className="text-white text-center text-[16px] py-6 border-t-2">
+          &copy; Copyright 2021 .Ojjomedia. All Right Reserved.
+        </p>
       </footer>
     </div>
   );
